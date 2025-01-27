ae390fcc1b11b974bf29ad58ff8fff901795a656
Stanley Gilstrap <Stangil44@Gmail.com>
2025-01-27 14:59:28 -0500

    Added EntityFramework for authorization

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using OnlineShop.Components;
 using ShopBackendLibrary.SqlDataAccess;
+using Microsoft.EntityFrameworkCore;
+using ShopBackendLibrary.Data;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "auth_token";
+        options.LoginPath = "/login";
+        options.Cookie.MaxAge = TimeSpan.FromMinutes(30);
+        options.AccessDeniedPath = "/access-denied";
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();//Passes Authentication state through the application
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +37,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
